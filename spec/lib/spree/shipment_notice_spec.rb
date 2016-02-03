972dcd3df2b433bcf2817e72c8de0100d2ea621c
stephen-puiszis <spuiszis@gmail.com>
2016-02-03 12:56:30 -0600

    Fix broken shipment_notice specs

--- spec/lib/spree/shipment_notice_spec.rb
+++ spec/lib/spree/shipment_notice_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 include Spree
 
-describe Spree::ShipmentNotice do
-  let(:notice) { ShipmentNotice.new(order_number:    'S12345', 
-                                    tracking_number: '1Z1231234') }
+describe Spree::ShipmentNotice, :focus do
+  let(:notice) do
+    ShipmentNotice.new(order_number:    'S12345',
+                       tracking_number: '1Z1231234')
+  end
 
-  context "#apply" do
-    context "shipment found" do
-      let(:shipment) { mock_model(Shipment, :shipped? => false) }
+  context '#apply' do
+    context 'shipment found' do
+      let(:shipment) { double(Shipment, shipped?: false) }
 
       before do
         Spree::Config.shipstation_number = :shipment
@@ -16,17 +18,17 @@ describe Spree::ShipmentNotice do
         shipment.should_receive(:update_attribute).with(:tracking, '1Z1231234')
       end
 
-      context "transition succeeds" do
+      context 'transition succeeds' do
         before do
           shipment.stub_chain(:reload, :update_attribute).with(:state, 'shipped')
           shipment.stub_chain(:inventory_units, :each)
           shipment.should_receive(:touch).with(:shipped_at)
         end
 
-        specify { notice.apply.should be_true }
+        specify { notice.apply.should eq(true) }
       end
 
-      context "transition fails" do
+      context 'transition fails' do
         before do
           shipment.stub_chain(:reload, :update_attribute)
                   .with(:state, 'shipped')
@@ -34,14 +36,14 @@ describe Spree::ShipmentNotice do
           @result = notice.apply
         end
 
-        specify { @result.should be_false }
+        specify { @result.should eq(false) }
         specify { notice.error.should_not be_blank }
       end
     end
 
-    context "using order number instead of shipment number" do
-      let(:shipment) { mock_model(Shipment, :shipped? => false) }
-      let(:order)    { mock_model(Order, shipment: shipment) }
+    context 'using order number instead of shipment number' do
+      let(:shipment) { double(Shipment, shipped?: false) }
+      let(:order)    { double(Order, shipment: shipment) }
 
       before do
         Spree::Config.shipstation_number = :order
@@ -51,26 +53,26 @@ describe Spree::ShipmentNotice do
         shipment.should_receive(:touch).with(:shipped_at)
       end
 
-      context "transition succeeds" do
+      context 'transition succeeds' do
         before { shipment.stub_chain(:reload, :update_attribute).with(:state, 'shipped') }
 
-        specify { notice.apply.should be_true }
+        specify { notice.apply.should eq(true) }
       end
     end
 
-    context "shipment not found" do
+    context 'shipment not found' do
       before do
         Spree::Config.shipstation_number = :shipment
         Shipment.should_receive(:find_by_number).with('S12345').and_return(nil)
         @result = notice.apply
       end
 
-      specify { @result.should be_false }
+      specify { @result.should eq(false) }
       specify { notice.error.should_not be_blank }
     end
 
-    context "shipment already shipped" do
-      let(:shipment) { mock_model(Shipment, :shipped? => true) }
+    context 'shipment already shipped' do
+      let(:shipment) { double(Shipment, shipped?: true) }
 
       before do
         Spree::Config.shipstation_number = :shipment
@@ -78,7 +80,7 @@ describe Spree::ShipmentNotice do
         shipment.should_receive(:update_attribute).with(:tracking, '1Z1231234')
       end
 
-      specify { notice.apply.should be_true }
+      specify { notice.apply.should eq(true) }
     end
   end
 end
