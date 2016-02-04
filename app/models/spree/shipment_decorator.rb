12c2b1faad236dcb8e4b5a26a9fd764827f14650
stephen-puiszis <spuiszis@gmail.com>
2016-02-03 18:27:31 -0600

    Fix broken shipment_decorator.rb specs

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,12 +1,23 @@
 Spree::Shipment.class_eval do
+  # TODO: research Spree::Shipment states and determine if this what we need
+  #   with inventory tracking turned on/off
   scope :exportable, -> { joins(:order).where('spree_shipments.state != ?', 'pending') }
 
+  # TODO: research Shipstation docs and Spree::Shipment and determine if this is
+  #   the right logic for determining what should be pulled
   def self.between(from, to)
-    joins(:order).where('(spree_shipments.updated_at > ? AND spree_shipments.updated_at < ?) OR (spree_orders.updated_at > ? AND spree_orders.updated_at < ?)',from, to, from, to)
+    joins(:order).where(
+      '(spree_shipments.updated_at > ? AND spree_shipments.updated_at < ?) OR
+      (spree_orders.updated_at > ? AND spree_orders.updated_at < ?)',
+      from, to, from, to
+    )
   end
 
 private
+
+  # TODO: determine if this is even still needed and how the new Spree::CartonMailer
   def send_shipped_email
-    Spree::ShipmentMailer.shipped_email(self).deliver if Spree::Config.send_shipped_email
+    Spree::CartonMailer.shipped_email(self).deliver if Spree::Config.send_shipped_email
   end
+
 end
--- spec/models/spree/shipment_spec.rb
+++ spec/models/spree/shipment_spec.rb
@@ -1,40 +1,59 @@
 require 'spec_helper'
+require 'timecop'
 
 describe Spree::Shipment do
-  context "between" do
-    before do
-      @active = []
+  context 'shipment_decorator methods' do
+    describe '.between' do
+      let(:now) { Time.now.utc }
 
-      create_shipment(updated_at: 1.day.ago, order: create(:order, updated_at: 1.day.ago))
-      create_shipment(updated_at: 1.day.from_now, order: create(:order, updated_at: 1.day.from_now))
-    
-      # Old shipment thats order was recently updated..
-      @active << create_shipment(updated_at: 1.week.ago, order: create(:order, updated_at: Time.now))
+      let!(:order_1) { create(:order) }
+      let!(:order_2) { create(:order) }
+      let!(:order_3) { create(:order) }
+      let!(:yesterday) { create_shipment(order: order_2) }
+      let!(:tomorrow) { create_shipment(order: order_2) }
+      let!(:old_shipment_recent_order_update) { create_shipment(created_at: now - 1.week, order: order_3) }
+      let!(:active_1) { create_shipment }
+      let!(:active_2) { create_shipment }
+      let(:query) { Spree::Shipment.between(now - 1.hour, now + 1.hours) }
 
-      @active << create_shipment(updated_at: Time.now)
-      @active << create_shipment(updated_at: Time.now)
+      # Use Timecop set #updated_at at specific times rather than manually settting them
+      #   as ActiveRecord will automatically set #updated_at timestamps even when attempting to
+      #   override them for Spree::Order instances
+      before do
+        Timecop.freeze(now - 1.day) do
+          order_1.touch
+          yesterday.touch
+        end
+
+        Timecop.freeze(now + 1.day) do
+          order_2.touch
+          tomorrow.touch
+        end
+
+        Timecop.freeze(now - 1.week) do
+          order_3.touch
+        end
+      end
+
+      it 'returns shipments based on shipments/orders updated_at within the given time range', :aggregate_failures do
+        expect(query.count).to eq(3)
+        expect(query).to match_array([old_shipment_recent_order_update, active_1, active_2])
+      end
     end
 
-    subject { Spree::Shipment.between(Time.now-1.hour, Time.now + 1.hours) }
+    describe '.exportable' do
+      let!(:pending) { create_shipment(state: 'pending') }
+      let!(:ready)   { create_shipment(state: 'ready')   }
+      let!(:shipped) { create_shipment(state: 'shipped') }
 
-    specify { should have(3).shipment }
+      let(:query) { Spree::Shipment.exportable }
 
-    specify { should == @active }
-  end
-
-  context "exportable" do
-    let!(:pending) { create_shipment(state: 'pending') }
-    let!(:ready)   { create_shipment(state: 'ready')   }
-    let!(:shipped) { create_shipment(state: 'shipped') }
-
-    subject { Spree::Shipment.exportable }
-
-    specify { should have(2).shipments }
-
-    specify { should include(ready)}
-    specify { should include(shipped)}
-
-    specify { should_not include(pending)}
+      it 'should have the expected shipment instances', :aggregate_failures do
+        expect(query.count).to eq(2)
+        expect(query).to eq([ready, shipped])
+        expect(query).to_not include(pending)
+      end
+    end
   end
 
   context "shipped_email" do
