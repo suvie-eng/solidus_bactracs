308c55931ea13cf362ab2b7847cd8374f118a02f
stephen-puiszis <spuiszis@gmail.com>
2016-02-11 16:03:09 -0600

    Add version.rb

--- lib/solidus_shipstation.rb
+++ lib/solidus_shipstation.rb
@@ -2,3 +2,9 @@ require 'solidus_core'
 require 'solidus_shipstation/engine'
 require 'spree/shipment_notice'
 require 'spree/map_query_string_app'
+
+module SolidusShipstation
+
+  VERSION = '1.0.0'.freeze
+
+end
