ae6042cf857555965cc42fb5fd3d4126978fdc13
Allison C. Reilly <acreilly3@gmail.com>
2016-12-05 09:30:35 -0500

    Require simplecov to fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
+# Run Coverage report
+require 'simplecov'
 SimpleCov.start do
   add_filter 'spec/dummy'
   add_group 'Controllers', 'app/controllers'
