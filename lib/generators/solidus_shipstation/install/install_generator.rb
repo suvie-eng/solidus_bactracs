80288c146e9545569c0bd46a53eb2075325ade99
Alberto Vena <kennyadsl@gmail.com>
2020-10-08 13:04:27 +0200

    Fix installer removing useless assets and migrations
    This extension has no assets or migrations to copy on the
    host application. This wrong behavior was also raising an
    error trying to install the extension since the files it
    was trying to copy do not exist.

--- lib/generators/solidus_shipstation/install/install_generator.rb
+++ lib/generators/solidus_shipstation/install/install_generator.rb
@@ -3,30 +3,6 @@
 module SolidusShipstation
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      class_option :auto_run_migrations, type: :boolean, default: false
-
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/solidus_shipstation/frontend/all.js', "//= require solidus_shipstation/frontend/solidus_shipstation\n"
-        append_file 'vendor/assets/javascripts/solidus_shipstation/backend/all.js', "//= require solidus_shipstation/backend/solidus_shipstation\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/solidus_shipstation/frontend/all.css', " *= require solidus_shipstation/frontend/solidus_shipstation\n", before: %r{\*/}, verbose: true
-        inject_into_file 'vendor/assets/stylesheets/solidus_shipstation/backend/all.css', " *= require solidus_shipstation/backend/solidus_shipstation\n", before: %r{\*/}, verbose: true
-      end
-
-      def add_migrations
-        run 'bin/rails railties:install:migrations FROM=solidus_shipstation'
-      end
-
-      def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
-        if run_migrations
-          run 'bin/rails db:migrate'
-        else
-          puts 'Skipping bin/rails db:migrate, don\'t forget to run it!' # rubocop:disable Rails/Output
-        end
-      end
     end
   end
 end
