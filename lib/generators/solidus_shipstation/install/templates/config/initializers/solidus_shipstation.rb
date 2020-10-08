905e6227bb6c20fc625ab979d5713d2a28ca8dac
Alberto Vena <kennyadsl@gmail.com>
2020-10-08 13:19:21 +0200

    Fix references to preferences in the doc
    Apparently, those are the real names of these preferences despite what was
    written in the README.

--- lib/generators/solidus_shipstation/install/templates/config/initializers/solidus_shipstation.rb
+++ lib/generators/solidus_shipstation/install/templates/config/initializers/solidus_shipstation.rb
@@ -2,18 +2,15 @@
 
 SolidusShipstation.configure do |config|
   # Choose between Grams, Ounces or Pounds.
-  config.shipstation_weight_units = "Grams"
+  config.weight_units = "Grams"
 
   # ShipStation expects the endpoint to be protected by HTTP Basic Auth.
   # Set the username and password you desire for ShipStation to use.
-  config.shipstation_username = "smoking_jay_cutler"
-  config.shipstation_password = "my-awesome-password"
-
-  # Turn SSL on/off for testing and development purposes.
-  config.shipstation_ssl_encrypted = !Rails.env.development?
+  config.username = "smoking_jay_cutler"
+  config.password = "my-awesome-password"
 
   # Capture payment when ShipStation notifies a shipping label creation.
-  # Set this to `true` and `require_payment_to_ship` to `false` if you
+  # Set this to `true` and `Spree::Config.require_payment_to_ship` to `false` if you
   # want to charge your customers at the time of shipment.
-  config.shipstation_capture_at_notification = false
+  config.capture_at_notification = false
 end
