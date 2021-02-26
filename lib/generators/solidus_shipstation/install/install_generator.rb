31d5cc3ed2b300fa49145e964968f3930a321ec8
Alessandro Desantis <desa.alessandro@gmail.com>
2021-02-26 13:04:49 +0100

    Fix generator structure and template location

--- lib/generators/solidus_shipstation/install/install_generator.rb
+++ lib/generators/solidus_shipstation/install/install_generator.rb
@@ -3,10 +3,15 @@
 module SolidusShipstation
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, type: :boolean, default: false
       source_root File.expand_path('templates', __dir__)
 
-      def generate_shipstation_configuration_file
-        copy_file 'config/initializers/solidus_shipstation.rb', 'config/initializers/solidus_shipstation.rb', skip: true
+      def copy_initializer
+        copy_file 'initializer.rb', 'config/initializers/solidus_shipstation.rb', skip: true
+      end
+
+      def add_migrations
+        run 'bin/rails railties:install:migrations FROM=solidus_shipstation'
       end
     end
   end
