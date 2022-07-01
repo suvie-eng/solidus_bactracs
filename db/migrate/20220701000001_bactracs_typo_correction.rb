8003292dd8ce1ef9d0b2a450d58eb78b70a436fa
NewAlexandria <NewAlexandria@users.noreply.github.com>
2022-07-01 13:16:48 -0400

    (#14) Add migration for bactracs_synced_at

---
+++ db/migrate/20220701000001_bactracs_typo_correction.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class BactracsTypoCorrection < ActiveRecord::Migration[5.2]
+  def change
+    if ActiveRecord::Base.connection.column_exists?(:spree_shipments, :backtracs_synced_at)
+      rename_column :spree_shipments, :backtracs_synced_at, :bactracs_synced_at
+    end
+  end
+end
